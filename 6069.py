2f85d33a1db5790462fc14278025fe6f25b6ca2d
inu <sinth92@hotmail.com>
2023-08-29 19:27:17 +0900

    72,73 need to fix tmr

---
+++ 6069.py
@@ -0,0 +1,12 @@
+sentence = input()
+
+if sentence == "A":
+    print("best!!")
+elif sentence == "B":
+    print("good!!")
+elif sentence == "C":
+    print("run!")
+elif sentence == "D":
+    print("slowly~")
+else:
+    print("what?")
---
+++ 6072.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+while True:
+    
+    if n>1:
+        n -= 1
+        print(n)
+    else:
+        break
